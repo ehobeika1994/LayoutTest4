de95197936654a13c44a00fabd8c92a7e2d85f16
Edmond Hobeika <eddiehobeika1994@hotmail.com>
2016-04-03 20:47:09 +0100

    Layout Test 4 - Android

---
+++ src/org/me/myandroidstuff/LayoutTest4Activity.java
@@ -0,0 +1,13 @@
+package org.me.myandroidstuff;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LayoutTest4Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
